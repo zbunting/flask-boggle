da5557f222fc2d6b467bd822bbc64fb38f40cc21
Zach Bunting <zvrbunting@gmail.com>
2024-04-26 15:26:52 -0700

    WIP test_app

--- test_app.py
+++ test_app.py
@@ -35,14 +35,53 @@ class BoggleAppTestCase(TestCase):
         with app.test_client() as client:
             response = client.post('/api/new-game')
             json = response.get_json()
-            
+
             # check if gameId is a string type
-            self.assertIsInstance(json["gameId"], str, "gameId is not a string")
-            
+            self.assertIsInstance(
+                json["gameId"], str, "gameId is not a string")
+
             # check that all rows in board are list types
             for row in json['board']:
                 self.assertIsInstance(row, list, "row is not a list")
-            
+
             # check if game was added to games dictionary
             self.assertEqual(len(games), 1, "games dictionary len is not one")
-            
+
+    def test_score_words(self):
+        """"""
+
+        with app.test_client() as client:
+            response = client.post('/api/new-game')
+            json = response.get_json()
+
+            game_id = json["gameId"]
+
+            game = games[game_id]
+            game.board = [["C", "A", "T"], ["O", "X", "X"], ["X", "G", "X"]]
+
+            response = client.post(
+                '/api/score-word',
+                json={"gameId": game_id,
+                      "word": "CTT"}
+            )
+
+            json = response.get_json()
+            self.assertEqual(json["result"], "not-word")
+
+            response = client.post(
+                '/api/score-word',
+                json={"gameId": game_id,
+                      "word": "DOG"}
+            )
+
+            json = response.get_json()
+            self.assertEqual(json["result"], "not-on-board")
+
+            response = client.post(
+                '/api/score-word',
+                json={"gameId": game_id,
+                      "word": "CAT"}
+            )
+
+            json = response.get_json()
+            self.assertEqual(json["result"], "ok")
