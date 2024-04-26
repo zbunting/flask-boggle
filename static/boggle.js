521247846ee79cdeef0dade64ab3db43f6aa5057
Zach Bunting <zvrbunting@gmail.com>
2024-04-26 16:57:36 -0700

    Brian's feedback.

--- static/boggle.js
+++ static/boggle.js
@@ -27,15 +27,22 @@ async function start() {
 function displayBoard(board) {
   $table.innerHTML = '';
   const $tableBody = $table.createTBody();
-  
-  for(let row = 0; row < board.length; row++){
+
+  // TODO: not themselves row and col, use y and x
+  // will want to distinguish between actual row and col vs the indices
+  // TODO: can just use for...of loops
+  for (let row = 0; row < board.length; row++) {
+
     const $row = $tableBody.insertRow();
-    for(let column = 0; column < board[row].length; column++){
+
+    for (let column = 0; column < board[row].length; column++) {
+
       const $letter = $row.insertCell();
       $letter.innerText = board[row][column];
     }
-  } 
-  
+
+  }
+
 }
 
 
@@ -46,17 +53,38 @@ function displayBoard(board) {
  */
 
 async function handleFormSubmit(evt) {
-  // TODO
+
+  evt.preventDefault();
+
+  //TODO: only use response to mean the response object
+  const word = $wordInput.value.toUpperCase();
+  const apiResponse = await submitWordToAPI(word);
+
+  displayOutcome(apiResponse.result, word);
+
+  $wordInput.value = '';
+
 }
 
-
-// TODO: add an event listener for the form submission
+// add an event listener for the form submission
+$form.addEventListener("submit", handleFormSubmit);
 
 
 /** Submit word to API and return result from the response. */
 
 async function submitWordToAPI(word) {
-  // TODO
+
+  const response = await fetch('/api/score-word', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ gameId, word })
+  })
+
+  //TODO: could just destructure and return the object you want
+  return await response.json();
+
 }
 
 
@@ -74,7 +102,28 @@ async function submitWordToAPI(word) {
  */
 
 function displayOutcome(result, word) {
-  // TODO
+
+  //TODO: $message.innerText = someFunction(result,word) to also be able
+  // to add a class
+  if (result === "not-word") {
+    $message.innerText = "Word not in dictionary!";
+  }
+
+  else if (result === "not-on-board") {
+    $message.innerText = "Word not on game board!";
+  }
+
+  else if (result === "word-is-dupe") {
+    $message.innerText = "Word has already been entered!";
+  }
+
+  else {
+    $message.innerText = "Valid word entered!";
+    const $playedWord = document.createElement('li');
+    $playedWord.innerHTML = word;
+    $playedWords.append($playedWord);
+  }
+
 }
 
 
